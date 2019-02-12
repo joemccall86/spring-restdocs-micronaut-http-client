ebf588eae4d98f289aef1362eab52743c325bdd1
Joe McCall <joemccall@intrepid-networks.com>
2019-02-12 07:26:10 -0500

    Implement documentation configurer as a http filter

--- src/main/java/spring/restdocs/micronaut/http/client/HttpClientDocumentationConfigurer.java
+++ src/main/java/spring/restdocs/micronaut/http/client/HttpClientDocumentationConfigurer.java
@@ -1,15 +1,32 @@
 package spring.restdocs.micronaut.http.client;
 
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.filter.ClientFilterChain;
+import io.micronaut.http.filter.HttpClientFilter;
+import org.reactivestreams.Publisher;
+import org.springframework.restdocs.RestDocumentationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.config.RestDocumentationConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClientDocumentationConfigurer extends
         RestDocumentationConfigurer<
                 HttpClientSnippetConfigurer,
                 HttpClientOperationPreprocessorsConfigurer,
-                HttpClientDocumentationConfigurer> {
+                HttpClientDocumentationConfigurer>
+        implements HttpClientFilter {
 
     private final HttpClientSnippetConfigurer httpClientSnippetConfigurer = new HttpClientSnippetConfigurer(this);
     private final HttpClientOperationPreprocessorsConfigurer httpClientOperationPreprocessorsConfigurer = new HttpClientOperationPreprocessorsConfigurer(this);
+
+    private RestDocumentationContextProvider contextProvider;
+
+    public HttpClientDocumentationConfigurer(RestDocumentationContextProvider contextProvider) {
+        this.contextProvider = contextProvider;
+    }
 
     @Override
     public HttpClientSnippetConfigurer snippets() {
@@ -19,5 +36,16 @@ public class HttpClientDocumentationConfigurer extends
     @Override
     public HttpClientOperationPreprocessorsConfigurer operationPreprocessors() {
         return httpClientOperationPreprocessorsConfigurer;
+    }
+
+    @Override
+    public Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
+        RestDocumentationContext context = this.contextProvider.beforeOperation();
+        Map<String, Object> configuration = new HashMap<>();
+        apply(configuration, context);
+        // TODO discover a way to pass the context and configuration to the execution chain so the HttpClientDocumentationFilter can make use of it
+        // A naive approach would be to place it in a ContextHolder singleton, but that's not thread-safe.
+
+        return chain.proceed(request);
     }
 }
--- src/main/java/spring/restdocs/micronaut/http/client/HttpClientDocumentationFilter.java
+++ src/main/java/spring/restdocs/micronaut/http/client/HttpClientDocumentationFilter.java
@@ -10,6 +10,7 @@ import org.reactivestreams.Publisher;
 import org.springframework.restdocs.generate.RestDocumentationGenerator;
 
 import java.util.Collections;
+import java.util.Map;
 
 public class HttpClientDocumentationFilter implements HttpClientFilter {
 
@@ -24,9 +25,14 @@ public class HttpClientDocumentationFilter implements HttpClientFilter {
 
         // WARNING: previous implementation had to make a copy of the response for documenting purposes because the stream was closed.
         // I may have to do this here as well.
+
+
+        // TODO grab the configuration map from wherever it is stored by the HttpClientDocumentationConfigurer.
+        Map<String, Object> configuration = Collections.emptyMap();
+
         return Publishers.then(
                 chain.proceed(request),
-                (response) -> delegate.handle(request, response, Collections.emptyMap())
+                (response) -> delegate.handle(request, response, configuration)
         );
     }
 }
